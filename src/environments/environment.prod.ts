14a218819e5351a4026d2bfdd8924ca1188bb340
Eulio <maildeblin@gmail.com>
2023-12-21 16:05:05 +0530

    Fixed a Issue cause Image or icons not load on some ISP --change asset raw location from github to bitbucket as some ISP don't like github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   craftingDataApi: 'https://us-east1-eco-crafting-api.cloudfunctions.net/eco-crafting-data',
   releasesUrl: 'https://api.github.com/repos/aritchie05/EcoCraftingTool/releases',
-  imageBaseUrl: 'https://raw.githubusercontent.com/aritchie05/EcoCraftingTool/master/src/assets/img/'
+  imageBaseUrl: 'https://bitbucket.org/ecocraftingcalculator/ecocraftingcalculator/raw/fc9edda16bad30f0177f918d81939fe74df60089/assets/img/'
 };
