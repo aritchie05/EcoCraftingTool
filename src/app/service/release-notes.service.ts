6f0eb78d3f0ec399716e4d27d5080312571e358a
Adam Ritchie <aritchie05@gmail.com>
2025-10-23 21:54:25 -0500

    Defer loading of releases

--- src/app/service/release-notes.service.ts
+++ src/app/service/release-notes.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, signal, WritableSignal} from '@angular/core';
+import {afterNextRender, Injectable, signal, WritableSignal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Release} from '../model/release';
 import {environment} from '../../environments/environment';
@@ -10,8 +10,12 @@ export class ReleaseNotesService {
 
   readonly releases: WritableSignal<Release[]> = signal([]);
 
-  constructor(httpClient: HttpClient) {
-    httpClient.get<Release[]>(environment.releasesUrl).subscribe(releases => this.releases.set(releases));
+  constructor(private http: HttpClient) {
+    afterNextRender(() => this.loadReleases());
+  }
+
+  private loadReleases(): void  {
+    this.http.get<Release[]>(environment.releasesUrl).subscribe(releases => this.releases.set(releases));
   }
 
 }
